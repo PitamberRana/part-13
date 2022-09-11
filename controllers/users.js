5f30529d5b98353f4d8adf2094499597a1553459
pitamberrana <pitamber11rana@gmail.com>
2022-09-11 10:23:54 +0545

    feat: 13c migrations

--- controllers/users.js
+++ controllers/users.js
@@ -1,14 +1,35 @@
 const router = require("express").Router();
 const { tokenExtractor } = require("../util/middleware");
 
-const { User, Note } = require("../models");
+const { User, Note, Team } = require("../models");
 
 router.get("/", async (req, res) => {
   const users = await User.findAll({
-    include: {
-      model: Note,
-      attributes: { exclude: ["userId"] },
-    },
+    include: [
+      {
+        model: Note,
+        attributes: { exclude: ["userId"] },
+      },
+      {
+        model: Note,
+        as: "marked_notes",
+        attributes: { exclude: ["userId"] },
+        through: {
+          attributes: [],
+        },
+        include: {
+          model: User,
+          attributes: ["name"],
+        },
+      },
+      {
+        model: Team,
+        attributes: ["name", "id"],
+        through: {
+          attributes: [],
+        },
+      },
+    ],
   });
 
   res.json(users);
@@ -24,9 +45,17 @@ router.post("/", async (req, res) => {
 });
 
 router.get("/:id", async (req, res) => {
-  const user = await User.findByPk(req.params.id);
+  const user = await User.findByPk(req.params.id, {
+    include: {
+      model: Note,
+    },
+  });
   if (user) {
-    res.json(user);
+    res.json({
+      username: user.username,
+      name: user.name,
+      note_count: user.notes.length,
+    });
   } else {
     res.status(404).end();
   }
--- models/index.js
+++ models/index.js
@@ -1,12 +1,25 @@
 const Note = require("./Note");
 const User = require("./User");
+const Team = require("./team");
+const Membership = require("./membership");
+const UserNotes = require("./user_notes");
 
 User.hasMany(Note);
 Note.belongsTo(User);
+
+User.belongsToMany(Team, { through: Membership });
+Team.belongsToMany(User, { through: Membership });
+
+User.belongsToMany(Note, { through: UserNotes, as: "marked_notes" });
+Note.belongsToMany(User, { through: UserNotes, as: "users_marked" });
+
 // Note.sync({alter: true});
 // User.sync({alter: true});
 
 module.exports = {
   Note,
   User,
+  Team,
+  Membership,
+  UserNotes,
 };
